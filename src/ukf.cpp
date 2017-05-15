340df0ffaa427f27e019938a78e986a930378008
heavy02011 <you@example.com>
2017-05-15 21:33:36 +0200

    moce some constants from function to class

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -239,23 +239,7 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
 
   //define spreading parameter
   double lambda_ = 3 - n_aug_;
-
-  //set example state
-  VectorXd x_ = VectorXd(n_x_);
-  x_ <<   5.7441,
-         1.3800,
-         2.2049,
-         0.5015,
-         0.3528;
-
-  //create example covariance matrix
-  MatrixXd P = MatrixXd(n_x_, n_x_);
-  P_ <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
-          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
-           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
-          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
-          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
-
+  
   //create augmented mean vector
   VectorXd x_aug_ = VectorXd(7);
 
@@ -296,7 +280,7 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
  ******************************************************************************/
 
   //print result
-  std::cout << "Xsig_aug = " << std::endl << Xsig_aug_ << std::endl;
+  //std::cout << "Xsig_aug = " << std::endl << Xsig_aug_ << std::endl;
 
   //write result
   *Xsig_out = Xsig_aug_;
